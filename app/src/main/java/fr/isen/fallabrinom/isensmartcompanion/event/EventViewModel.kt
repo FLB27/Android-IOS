659b64b3880d1bba693ecd550809b61308f6747f
FLB27 <fallabrinomaxime@gmail.com>
2025-03-24 14:41:50 +0100

    Récupération des événements sur le serveur du prof + Maj des notifications en fonction du nombre de non lus (Avec Retrofit géré dans un fichier Retrofit.kt)

--- app/src/main/java/fr/isen/fallabrinom/isensmartcompanion/event/EventViewModel.kt
+++ app/src/main/java/fr/isen/fallabrinom/isensmartcompanion/event/EventViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import fr.isen.fallabrinom.isensmartcompanion.Event
+import fr.isen.fallabrinom.isensmartcompanion.API.RetrofitInstance
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
