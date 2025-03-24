217ba786ff249667059754b83ae98da6c1d4454e
FLB27 <fallabrinomaxime@gmail.com>
2025-03-24 14:35:43 +0100

    Récupération des événements sur le serveur du prof + Maj des notifications en fonction du nombre de non lus

--- app/src/main/java/fr/isen/fallabrinom/isensmartcompanion/event/EventViewModel.kt
+++ app/src/main/java/fr/isen/fallabrinom/isensmartcompanion/event/EventViewModel.kt
@@ -39,12 +39,20 @@ class EventViewModel : ViewModel() {
         )
     ))*/
     private val _events = MutableLiveData<List<Event>>() // Stocke la liste des événements
+    val events: LiveData<List<Event>> = _events //classe qui encapsule des données et permet à plusieurs observateurs (comme des composants UI) de recevoir des mises à jour automatiquement lorsque les données changent
+    private val _eventCount = MutableLiveData<Int>(_events.value?.size ?: 0) // Initialise le compteur d'événements à la taille de la liste
+    //events.value renvoie la liste d’événements stockée dans _events
+    // ?.size opérateur de sécurité de nullité si size = null ça renvoie pas d'erreur car renvoie null
+    // ?: 0 si elle est null donc vide on renvoie 0 pour forcer
+    val eventCount: LiveData<Int> get() = _eventCount
+
 
     fun fetchEvents() {
         RetrofitInstance.api.getEvents().enqueue(object : Callback<List<Event>> { //enqueue permet de faire l'action en arrière plan sans bloquer l'interface
             override fun onResponse(call: Call<List<Event>>, response: Response<List<Event>>) {
                 if (response.isSuccessful) { // Vérifie que la réponse est OK (200)
                     _events.value = response.body() // Met à jour les événements
+                    updateEventCount(_events.value?.size ?: 0) //Mise à jour du nombre de notifs
                 }
             }
 
@@ -54,18 +62,10 @@ class EventViewModel : ViewModel() {
         })
     }
 
-    val events: LiveData<List<Event>> = _events //classe qui encapsule des données et permet à plusieurs observateurs (comme des composants UI) de recevoir des mises à jour automatiquement lorsque les données changent
 
     fun removeEvent(eventId: String) { //removeEvent(eventId) filtre la liste et met à jour _events, ce qui déclenche un re-render de l’UI
         _events.value = _events.value?.filter { it.id != eventId } // Supprime l'événement avec l'ID donné
     }
-
-    private val _eventCount = MutableLiveData<Int>(events.value?.size ?: 0) // Initialise le compteur d'événements à la taille de la liste
-    //events.value renvoie la liste d’événements stockée dans _events
-    // ?.size opérateur de sécurité de nullité si size = null ça renvoie pas d'erreur car renvoie null
-    // ?: 0 si elle est null donc vide on renvoie 0 pour forcer
-
-    val eventCount: LiveData<Int> get() = _eventCount
 
     fun updateEventCount(count: Int) {
         _eventCount.value = count
